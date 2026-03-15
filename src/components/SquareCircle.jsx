a8d5386a96113f0d2f06dc0d56dda6d24a1e6eff
P Sujith Bhatt <sujithdon00797@gmail.com>
2026-03-15 15:00:37 +0530

    square circle comp imp with styling & added to App

---
+++ src/components/SquareCircle.jsx
@@ -0,0 +1,12 @@
+import "./SquareCircle.css";
+
+export const SquareCircle = () => {
+  return (
+    <div className="card">
+      <h1>Square & Circle</h1>
+      <div className="square">
+        <div className="circle"></div>
+      </div>
+    </div>
+  );
+};
